a853d2b45588caf547fb23e2c8b78e5be4343035
dylan <dylanwozny14@gmail.com>
2022-05-29 00:02:53 -0600

    html skeleton added.optimized svgs

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ Dylan wozny's static portfolio website
 
 Using cmd line for sass compiling. No extension.
 
+compile command: 'npm start'
+
 Json dependencies from https://thinkdobecreate.com/articles/minimum-static-site-sass-setup/
 
 ## GITHUB
@@ -15,3 +17,11 @@ step 1: git add .
 step 2: git commit -m ""
 
 step 2: git push -u origin main
+
+## DEV Dependencies
+
+SVGO - cleaning up inline svg code. Like removing xmlns
+
+https://github.com/svg/svgo
+
+svgo -f ./src/assets/svgs -o ./src/assets/img
---
+++ svgo.config.js
@@ -0,0 +1,22 @@
+// svgo.config.js
+module.exports = {
+  js2svg: {
+    indent: 2, // string with spaces or number of spaces. 4 by default
+    pretty: true, // boolean, false by default
+  },
+  plugins: [
+    // set of built-in plugins enabled by default
+    "preset-default",
+
+    // enable built-in plugins by name
+    "removeXMLNS",
+
+    // or by expanded notation which allows to configure plugin
+    {
+      name: "sortAttrs",
+      params: {
+        xmlnsOrder: "alphabetical",
+      },
+    },
+  ],
+};
