789516e6deb3f35cdb7cbc3528620768e30acd5f
dylan <dylanwozny14@gmail.com>
2022-06-03 18:40:27 -0600

    built vertical navbar menu

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+var button = document.querySelector(".nav-btn");
+console.log(button);
+
+button.addEventListener("click", () => {
+  document.querySelector("nav").classList.toggle("nav-show");
+});
