370dc0f16cd644992d03e06c892713dcd5efcc2f
dylan <dylanwozny14@gmail.com>
2022-11-09 18:38:37 -0700

    added next.js and styled components to svg tool list in to do app

--- src/wedding-website/js/hotels.js
+++ src/wedding-website/js/hotels.js
@@ -1,32 +1,27 @@
 // hotels
+// hotels
 let hotelDomList = document.querySelector(".location-card-container");
 
-fetch(
-  "https://hotels-com-provider.p.rapidapi.com/v1/hotels/nearby?locale=en_US&sort_order=BEST_SELLER&latitude=42.518437&adults_number=1&checkout_date=2022-09-27&currency=CAD&checkin_date=2022-09-26&longitude=18.656604&guest_rating_min=3&accommodation_ids=20%2C8%2C15%2C5%2C1&star_rating_ids=3%2C4%2C5&amenity_ids=527%2C2063&price_min=6&page_number=1&theme_ids=14%2C27%2C25&price_max=900&children_ages=4%2C0%2C15",
-  {
-    method: "GET",
-    headers: {
-      "x-rapidapi-key": "d7edd54c49msh58d604eefb5b16ap1b182ajsn18a1e843a3db",
-      "x-rapidapi-host": "hotels-com-provider.p.rapidapi.com",
-    },
-  }
-)
-  .then((response) => {
-    return response.json();
-  })
-  .then((results) => {
-    console.log(results.searchResults.results);
-    // store list of hotels in var
-    let fiveHotels = results.searchResults.results;
-    console.log(fiveHotels);
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "456889fbd7mshbf0f71b95261d81p17ea1djsnde60c2fb4269",
+    "X-RapidAPI-Host": "hotels-com-provider.p.rapidapi.com",
+  },
+};
 
-    // //grab details of each hotel using function detailed below !
-    // foreach()  or MAP() can be used
+fetch(
+  "https://hotels-com-provider.p.rapidapi.com/v1/hotels/nearby?locale=en_US&sort_order=BEST_SELLER&latitude=42.518437&adults_number=1&checkout_date=2023-09-27&currency=CAD&checkin_date=2023-09-26&longitude=18.656604&guest_rating_min=3&accommodation_ids=20%2C8%2C15%2C5%2C1&star_rating_ids=3%2C4%2C5&amenity_ids=527%2C2063&price_min=6&page_number=1&theme_ids=14%2C27%2C25&price_max=900&children_ages=4%2C0%2C15",
+  options
+)
+  .then((response) => response.json())
+  .then((response) => {
+    console.log(response.searchResults.results);
+    let fiveHotels = response.searchResults.results;
+    console.log(fiveHotels);
     fiveHotels.map((element) => getAndRenderHotel(element));
   })
-  .catch((err) => {
-    console.error(err);
-  });
+  .catch((err) => console.error(err));
 
 // ---go through json and render the hotel on page----
 getAndRenderHotel = (element) => {
